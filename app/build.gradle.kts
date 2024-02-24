0aad8cf59fcf3c42f41c0167bdd4f6499d560eb0
androidbulb <rajendrakumar4302@gmail.com>
2024-02-24 15:29:25 +0530

    Feat- Improved OptionDialog as Material3 UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,5 +54,7 @@ dependencies {
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
   implementation("com.github.androidbulb:CircleImageViewLibrary:1.0")
+  implementation("com.google.android.material:material:1.5.0-alpha01")
+
 
 }

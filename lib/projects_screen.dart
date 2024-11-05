6490af50a49d4f739b7575d198465a3463759501
flutterdevil <araviationprivatelimited@gmail.com>
2024-11-05 10:32:36 +0530

    UI-Projects UI Improved (#82)
    * Fix-App Logo Picker
    
    * UI-Projects UI Improved
    
    ---------
    
    Signed-off-by: flutterdevil <araviationprivatelimited@gmail.com>

--- lib/projects_screen.dart
+++ lib/projects_screen.dart
@@ -35,13 +35,49 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        elevation: 4.0,
       ),
 
-      /// Display list of projects or a message if empty
+      /// Display list of projects as cards or a message if empty
       body: allproject.isNotEmpty
           ? ListView.builder(
+              padding: const EdgeInsets.all(10.0),
               itemCount: allproject.length,
               itemBuilder: (_, index) {
+                final appLogoPath =
+                    allproject[index][DbHandler.COLUMN_APP_LOGO_PATH];
+
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 8.0),
+                  elevation: 1.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: ListTile(
+                    leading: Padding(
+                      padding: const EdgeInsets.all(
+                          1.0), // Adds an 8-pixel margin around the avatar
+                      child: appLogoPath != null && appLogoPath.isNotEmpty
+                          ? CircleAvatar(
+                              backgroundImage: FileImage(File(appLogoPath)),
+                              radius: 25, // Sets the size of the avatar
+                            )
+                          : const CircleAvatar(
+                              radius: 25,
+                              child: Icon(
+                                Icons.image,
+                                size:100, // Size of the placeholder icon inside the CircleAvatar
+                              ),
+                            ),
+                    ), // Placeholder if no logo
+                    title: Text(
+                      allproject[index][DbHandler.COLUMN_APP_NAME],
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+
                 final appLogoPath = allproject[index][DbHandler.COLUMN_APP_LOGO_PATH];
                 
                 return ListTile(
@@ -53,14 +89,22 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
           : const SizedBox.shrink()
       : const SizedBox.shrink(),
                     title: Text(allproject[index][DbHandler.COLUMN_APP_NAME]),
+
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        const SizedBox(height: 4.0),
                         Text(allproject[index][DbHandler.COLUMN_PROJECT_NAME]),
                         Text(allproject[index]
                             [DbHandler.COLUMN_APP_PACKAGE_NAME]),
                       ],
-                    ));
+                    ),
+                    trailing: Text(
+                      allproject[index][DbHandler.COLUMN_PROJECT_ID].toString(),
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),
+                );
               },
             )
           : const Center(
@@ -71,7 +115,6 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
           FocusScope.of(context).unfocus(); // Dismiss the keyboard
           Get.to(() => const CreateProject()); // Navigate to CreateProject
         },
-
         child: const Icon(Icons.add),
       ),
     );
